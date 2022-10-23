cf59d6fbed0c0a450117ef333cae78e5ad403ba5
Sidsha242 <sdemail2244@gmail.com>
2022-10-23 16:02:23 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import user2 from "./svg/exodus-logo.svg";
+import CountdownTimer from "./Components/CountdownTimer/CountdownTimer";
+import { Link } from "react-router-dom";
+
+
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+function Home() {
+  return (
+    <div className="home-page">
+      <div className="logo">
+        <img className="logo-pic" src={user2} alt="logo"></img>
+      </div>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+      <div className="cont-1">
+        We are almost there
+      </div>
+      <CountdownTimer
+        countdownTimestampMs={1670091600000} />
+      <Buttons />
+    </div >
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Buttons() {
+  return (
+    <div className="button-comp">
+      {/* <Link to=""> */}
+      <button className="button-home">
+        TIMELINE
+      </button>
+      {/* </Link> */}
+      {/* <Link to=""> */}
+      <button className="button-home">
+        RULES
+      </button>
+      {/* </Link> */}
+    </div>
+  )
+}
+
+
+
+createRoot(document.getElementById("root")).render(<Home />);
+//export default Home;
